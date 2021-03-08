faede7c37e47d637be317525139b322e6a2afae5
rmotaop <rmotaop@gmail.com>
2021-03-08 18:25:40 -0300

    Section

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -21,3 +21,7 @@ INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0
 INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha HTML', 'Trilha principal do curso', 1, 'https://th.bing.com/th/id/OIP.XOcIn1C4klFDj1VH97Ve8gHaHa?pid=ImgDet&rs=1', 1, 1);
 INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Forum HTML', 'Tire suas dúvidas', 2, 'https://th.bing.com/th/id/OIP.XOcIn1C4klFDj1VH97Ve8gHaHa?pid=ImgDet&rs=1', 2, 1);
 INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Lives', 'Lives exclusivas para a turma', 3, 'https://th.bing.com/th/id/OIP.XOcIn1C4klFDj1VH97Ve8gHaHa?pid=ImgDet&rs=1', 0, 1);
+
+INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 1', 'Nesse capítulo vamos começar', 1, 'https://th.bing.com/th/id/OIP.XOcIn1C4klFDj1VH97Ve8gHaHa?pid=ImgDet&rs=1', 1, null);
+INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 2', 'Nesse capítulo vamos continuar', 2, 'https://th.bing.com/th/id/OIP.XOcIn1C4klFDj1VH97Ve8gHaHa?pid=ImgDet&rs=1', 1, 1);
+INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 3', 'Nesse capítulo vamos finalizar', 3, 'https://th.bing.com/th/id/OIP.XOcIn1C4klFDj1VH97Ve8gHaHa?pid=ImgDet&rs=1', 1, 2);
