8eeb32463123aa722c0db4c22dbf15d4fe640fb2
rmotaop <rmotaop@gmail.com>
2021-03-11 18:30:51 -0300

    Custom service level authorization

--- backend/src/main/java/com/devsuperior/dslearnbds/services/UserService.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/services/UserService.java
@@ -18,14 +18,18 @@ import com.devsuperior.dslearnbds.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService implements UserDetailsService {
-	@Autowired
-	private UserRepository repository;
 	
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 	
+	@Autowired
+	private UserRepository repository;
 	
+	@Autowired
+	private AuthService authService;
+		
 	@Transactional(readOnly = true)
 	public UserDTO findById(Long id) {
+		authService.validateSelfOrAdmin(id);
 		Optional<User> obj = repository.findById(id);
 		User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity Not Found"));
 		return new UserDTO(entity);
